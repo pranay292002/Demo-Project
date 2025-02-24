9b8c932dd59bb0ef867135688757e06402de8231
Shivraj_prachande <149994778+ShivrajPrachande21@users.noreply.github.com>
2025-02-24 11:59:21 +0530

    added radio buttons

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,6 +5,8 @@ const SideBar = () => {
     <div>
         <form action="">
             <input type="text" placeholder='Enter name' />
+            <input type="radio" name="gender" id="" />
+            <input type="radio" name="gender" id="" />
             <button type='sumbit'>Sumbit</button>
         </form>
     </div>
