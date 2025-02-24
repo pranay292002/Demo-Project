a1429591ace67f797f4ae4f1267a82cb63f47d6e
pranay292002 <pranayparbate2002@gmail.com>
2025-02-24 12:22:09 +0530

    added age input

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,8 +4,10 @@ const SideBar = () => {
   return (
     <div>
         <form action="">
+
             <input type="text" placeholder='Enter name' />
             <input type='text' placeholder='Last Name'/>
+             <input type="range" />
             <button type='sumbit'>Sumbit</button>
         </form>
     </div>
