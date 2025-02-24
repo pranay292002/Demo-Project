e2459d45901da62e977bc320d11ac8af8c9f289a
pranay292002 <pranayparbate2002@gmail.com>
2025-02-24 13:03:50 +0530

    added stashed code

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -9,12 +9,12 @@ const SideBar = () => {
           <input type="text" placeholder="Last Name" />
           <input type="range" />
           <button type="sumbit">Sumbit</button>
-            <input type="text" placeholder='Enter name' />
-            <input type='text' placeholder='Last Name'/>
-            <input type="radio" name="gender" id="" />
-            <input type="radio" name="gender" id="" />
-            <button type='sumbit'>Sumbit</button>
-<h1></h1>
+          <input type="text" placeholder="Enter name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="radio" name="gender" id="" />
+          <input type="radio" name="gender" id="" />
+          <button type="sumbit">Sumbit</button>
+          <h1></h1>
         </form>
       </div>
       <div>menubar</div>
