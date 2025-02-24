94f4c3f5eb14c13c34d413f525e6515c6c2b425f
Shivraj_prachande <149994778+ShivrajPrachande21@users.noreply.github.com>
2025-02-24 12:21:23 +0530

    Added Gender Radio buttons

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,7 +6,10 @@ const SideBar = () => {
         <form action="">
             <input type="text" placeholder='Enter name' />
             <input type='text' placeholder='Last Name'/>
+            <input type="radio" name="gender" id="" />
+            <input type="radio" name="gender" id="" />
             <button type='sumbit'>Sumbit</button>
+
         </form>
     </div>
   )
